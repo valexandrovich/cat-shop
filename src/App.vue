9836502ffd9391d66d0ddc4a0cafb6f2a1e36bd5
Valerii Alexandrovich <beloenko23@gmail.com>
2023-11-01 00:23:16 +0200

    cats

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script setup>
 
+
+import {ref} from "vue";
+
+const cats = ref([
+
+
+  {id: 1, name: 'Пуська', age: 3, weight: 5,  img: '/cat1.png'},
+  {id: 2, name: 'Муська', age: 6, weight: 7,  img: '/cat2.png'},
+  {id: 3, name: 'Лапуська', age: 5, weight: 5,  img: '/cat3.png'},
+  {id: 4, name: 'Вадим', age: 76, weight: 77,  img: '/cat4.png'},
+
+])
+
+
 </script>
 
 <template>
-<div class="bg-amber-300">Кіт-шоп</div>
+<div class="flex flex-col justify-center">
+  <div class="flex flex-row justify-center gap-2 flex-1 bg-amber-200 py-6">
+    <img src="@/assets/img/cat_icon.png" alt="" class="w-8">
+    <span class="font-bold text-2xl">Купи кітика</span>
+  </div>
+
+
+  <div class="flex flex-row flex-wrap gap-4">
+    <div v-for="cat in cats" :key="cat.id" class="flex flex-col justify-center bg-gray-200 p-4">
+      <img :src="cat.img" alt="" class="w-64 h-64">
+      <span class="text-lg">Ім'я: <strong>{{cat.name}}</strong></span>
+      <span class="">Вік <strong>{{cat.age}}</strong> років</span>
+      <span class="">Вага <strong>{{cat.weight}}</strong> кг</span>
+      <button class="bg-amber-300 py-2 px-6 font-semibold">Придбати</button>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: { fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },},
   },
   plugins: [],
 }
